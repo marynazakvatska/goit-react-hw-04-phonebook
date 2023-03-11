7fe58a4e53e7046f020ec0f88283d1a3650a8724
marynazakvatska <maryna.zakvatska@gmail.com>
2023-03-11 23:05:25 +0100

    done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,87 +2,6 @@ import css from './Form.module.css'
 import {useState} from "react";
 import PropTypes from 'prop-types';
 
-/* 
-export default class Form extends Component {
-    state = {
-        name: '',
-   number: ''
-    }
-
-
-      handleNameChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState(
-       { [name]: value })
-    }
-    
-    handleSubmit = e => {
-        e.preventDefault()
-        this.props.onSubmit(this.state) //передает prop onSubmit Form значение state, передает значение state наверх в App
-        
-        this.reset()
-    }
-
-
-    reset = () => {
-    this.setState({name: '', number: ''})
-}
-
-    render() {
-        return (
-             
-           <form className={css.form} onSubmit={this.handleSubmit}>  
-             <label className={css.label}>Name 
-               <input className={css.input}
-                 value={this.state.name}
-        onChange={this.handleNameChange}
-           type="text"
-           name="name"
-           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                      
-                style={{
-                marginLeft: 20,
-             width: "300px",
-           height: "40px"}}
-               />
-                </label>
-            
-             
- <label className={css.label}> Number 
-               <input className={css.input}
-                   value={this.state.number}
-        onChange={this.handleNameChange}
-  type="tel"
-  name="number"
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                
-                style={{
-                marginLeft: 20,
-             width: 300,
-           height: 40}}
-/>
- </label>
-  <button className={css.btn} type="submit">Add contact</button>
-           </form>  
-           
-        )
-    }
-}
-
-
-Form.propTypes = {
-  
-  onChange: PropTypes.func,
-  onSubmit: PropTypes.func,
-  value:  PropTypes.string,
-
-} */
-
-
 
 
 export default function Form ({onSubmit}) {
@@ -97,9 +16,7 @@ export default function Form ({onSubmit}) {
 
     const handleNameChange = e => {
     const { name, value } = e.currentTarget;
-  /*   this.setState(
-       { [name]: value }) */
-    /*   console.log(name) */
+ 
       switch (name) {
         case 'name':
           setName(value);
@@ -114,9 +31,6 @@ export default function Form ({onSubmit}) {
     
    const handleSubmit = e => {
         e.preventDefault()
-       /*  this.props.onSubmit(this.state) */ //передает prop onSubmit Form значение state, передает значение state наверх в App
-     /*   this.props.onSubmit(state) */
-     /*  console.log( name, number) */
     
     onSubmit({name, number})
         reset()
@@ -124,7 +38,7 @@ export default function Form ({onSubmit}) {
 
 
    const reset = () => {
-     /*  this.setState({name: '', number: ''}) */
+    
      setName('');
      setNumber('')
      
@@ -133,7 +47,7 @@ export default function Form ({onSubmit}) {
    
         return (
              
-          /*    <form className={css.form} onSubmit={handleSubmit}>   */
+        
           <form className={css.form} onSubmit={handleSubmit}>  
              <label className={css.label}>Name 
                <input className={css.input}
